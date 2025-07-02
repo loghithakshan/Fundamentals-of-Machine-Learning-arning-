100a7b710bf9c7693ebe0f8068c46c4f569e79f8
Loghithakshan <159217940+loghithakshan@users.noreply.github.com>
2025-07-02 09:50:13 +0530

    Experiment 12.py

--- Experiment 12.py
+++ Experiment 12.py
@@ -7,7 +7,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
 # Load dataset
-iris = pd.read_csv("C:\\Users\\sri\\Desktop\\iris.csv")
+iris = pd.read_csv("C:\\Users\\loghi\\Desktop\\iris.csv")
 print(iris.head()) 
 print()
 print(iris.describe())
